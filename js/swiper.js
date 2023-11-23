4edc5098e65f25b2327f8520e05f153c111cd456
H4ppy <95917527+H4ppYEndLess@users.noreply.github.com>
2023-11-23 18:32:59 +0200

    ebat ya zdelav this huetu

---
+++ js/swiper.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+    navigation:{
+        nextEl:'.slider__next',
+        prevEl:'.slider__prev'
+    },
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<div class="' + className + ' .bullet">' + '<svg  width="60px" height="60px"><use  href="./images/svg/icons.svg#'+(index+1)+'"></use></svg></div>';
+          },
+      },
+      loop:true,
+      effect:'fade',
+      fadeEffect:{
+        crossFade:true
+      },
+
+});
